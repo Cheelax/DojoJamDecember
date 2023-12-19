9a859326f1e7df9b1cdeb499254364502baf46a0
0x-alx <alx.charbo@gmail.com>
2023-12-19 20:07:42 +0100

    added fake data

---
+++ frontend/src/utils/adventurerList.ts
@@ -0,0 +1,43 @@
+export const adventurerList = [
+    {
+        name: 'Player 1',
+        value: [
+            { name: 'strength', type: 'core::integer::u8', value: '2' },
+            { name: 'dexterity', type: 'core::integer::u8', value: '1' },
+            { name: 'vitality', type: 'core::integer::u8', value: '0' },
+        ],
+    },
+    {
+        name: 'Player 2',
+        value: [
+            { name: 'strength', type: 'core::integer::u8', value: '0' },
+            { name: 'dexterity', type: 'core::integer::u8', value: '1' },
+            { name: 'vitality', type: 'core::integer::u8', value: '2' },
+        ],
+    },
+    {
+        name: 'Player 3',
+        value: [
+            { name: 'strength', type: 'core::integer::u8', value: '0' },
+            { name: 'dexterity', type: 'core::integer::u8', value: '2' },
+            { name: 'vitality', type: 'core::integer::u8', value: '1' },
+        ],
+    },
+    {
+        name: 'Player 4',
+        value: [
+            { name: 'strength', type: 'core::integer::u8', value: '1' },
+            { name: 'dexterity', type: 'core::integer::u8', value: '2' },
+            { name: 'vitality', type: 'core::integer::u8', value: '0' },
+        ],
+    },
+    {
+        name: 'Player 5',
+        value: [
+            { name: 'strength', type: 'core::integer::u8', value: '2' },
+            { name: 'dexterity', type: 'core::integer::u8', value: '0' },
+            { name: 'vitality', type: 'core::integer::u8', value: '0' },
+        ],
+    },
+
+];
