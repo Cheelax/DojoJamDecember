95da84230f911bb1254ec09ce1233d4e69f3ada6
0x-alx <alx.charbo@gmail.com>
2023-12-24 12:13:10 +0100

    Add new state variables and setters to store.ts

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,15 +1,24 @@
 import { create } from 'zustand';
 import { NetworkLayer } from "./dojo/createNetworkLayer";
+import { AdventurerType } from "./utils/adventurerList";
 
 export type Store = {
     networkLayer: NetworkLayer | null;
+    loggedIn: boolean;
+    setLoggedIn: (loggedIn: boolean) => void;
+    username: string;
+    setUsername: (username: string) => void;
+    selectedAdventurer: {} | null;
+    setSelectedAdventurer: (adventurer: AdventurerType | null) => void;
 };
 
-export const store = create<Store>(() => ({
+export const store = create<Store>((set) => ({
     networkLayer: null,
+    loggedIn: false,
+    setLoggedIn: (loggedIn: boolean) => set(() => ({ loggedIn })),
+    username: "",
+    setUsername: (username: string) => set(() => ({ username })),
+    selectedAdventurer: null,
+    setSelectedAdventurer: (adventurer: AdventurerType | null) => set(() => ({ selectedAdventurer: adventurer })),
 }));
 
-export const useUIStore = create((set) => ({
-    loggedIn: false,
-    setLoggedIn: () => set(() => ({ loggedIn: true })),
-}));
